0bdca45cf73fb53eb52e25b6a97f9974a9eb4ca8
wfrankmulder <w.frank.mulder@gmail.com>
2015-04-09 10:24:04 +0200

    Create wekker.asm

---
+++ wekker.asm
@@ -0,0 +1,88 @@
+.include "m32def.inc"
+
+.def hour = r16
+.def minute = r17
+.def second = r18
+.def saveSR = r19
+.def temp = r20
+.def temp2 = r21
+
+.org 0x0000
+rjmp init
+
+.org OC1Aaddr
+rjmp ONE_SECOND_TIMER
+
+init:
+	ldi r16, high(RAMEND) ; init stack pointer
+	out SPH, r16
+	ldi r16, low(RAMEND)
+	out SPL, r16
+
+	ldi temp, high(43200) ; 1 sec = (256 / 11059200) * 43200
+	out OCR1AH, temp
+	ldi temp, low(43200)
+	out OCR1AL, temp
+
+	// Init UART
+	clr temp;
+	out UBRRH, temp
+	ldi temp, 35 ; 19200 baud
+	out UBRRL, temp
+	; set frame format : asynchronous, parity disabled, 8 data bits, 1 stop bit
+	ldi temp, (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0)
+	out UCSRC, temp
+	; enable receiver & transmitter
+	ldi temp, (1 << RXEN) | (1 << TXEN)
+	out UCSRB, temp
+
+	//prescaler 256 en CTC mode
+	ldi temp, (1 << CS12) | (1 << WGM12)
+	out TCCR1B, temp
+
+	//enable interrupt
+	ldi temp, (1 << OCIE1A)
+	out TIMSK, temp
+
+	clr temp
+	ldi temp2, 0b00110110
+	sei
+
+loop:
+	clr temp
+	out UDR, temp2
+	rjmp loop
+
+output:
+	cpi temp, 8
+	brlt verder
+	ret
+	verder:
+	out UDR, temp2
+	inc temp
+	rjmp output
+
+increment_time:
+	inc second
+	cpi second, 60
+	brlt minute_continue
+	clr second
+	inc minute
+	minute_continue:
+
+	cpi minute, 60
+	brlt hour_continue
+	clr minute
+	inc hour
+	cpi hour, 24
+	brlt hour_continue
+	clr hour
+	hour_continue:
+	ret
+
+ONE_SECOND_TIMER:
+	in saveSR, SREG
+	in r16, PORTB
+	rcall increment_time
+	out SREG, saveSR
+	reti
